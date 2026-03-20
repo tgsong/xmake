144688ea6acdae672a731532861886c777057e1b
ruki <waruqi@gmail.com>
2026-03-20 22:36:38 +0800

    improve pinvoke

--- tests/projects/csharp/pinvoke/src/app/Program.cs
+++ tests/projects/csharp/pinvoke/src/app/Program.cs
@@ -1,12 +1,16 @@
 using System.Runtime.InteropServices;
 
-[DllImport("mathlib")]
-static extern int add(int a, int b);
-
-[DllImport("mathlib")]
-static extern int multiply(int a, int b);
-
-int sum = add(3, 4);
-int product = multiply(5, 6);
+int sum = NativeMethods.add(3, 4);
+int product = NativeMethods.multiply(5, 6);
 Console.WriteLine($"add(3,4)={sum}");
 Console.WriteLine($"multiply(5,6)={product}");
+
+internal static class NativeMethods {
+    private const string NativeLib = "mathlib";
+
+    [DllImport(NativeLib)]
+    internal static extern int add(int a, int b);
+
+    [DllImport(NativeLib)]
+    internal static extern int multiply(int a, int b);
+}
