f4513cc28aa778e1ad0d050511cc7e295fe81e32
ruki <waruqi@gmail.com>
2026-03-29 22:31:50 +0800

    update to lua5.5

--- core/src/xmake/os/sleep.c
+++ core/src/xmake/os/sleep.c
@@ -35,7 +35,7 @@
  */
 tb_int_t xm_os_sleep(lua_State *lua) {
     tb_assert_and_check_return_val(lua, 0);
-    tb_long_t interval = (tb_long_t)luaL_checklong(lua, 1);
+    tb_long_t interval = (tb_long_t)luaL_checkinteger(lua, 1);
     if (interval >= 0) {
         tb_msleep(interval);
     }
