388c1813adcb65e09828cdbc4497c931b504d968
ruki <waruqi@gmail.com>
2026-04-24 23:20:44 +0800

    fix rust build

--- tests/projects/rust/static_library/test.lua
+++ tests/projects/rust/static_library/test.lua
@@ -1,6 +1,13 @@
 function main(t)
     if is_host("macosx") and os.arch() ~= "arm64" then
         t:build()
+        local targetfile = os.files("build/**/release/test")[1]
+        assert(targetfile and os.isfile(targetfile), "target file not found!")
+        local mtime = os.mtime(targetfile)
+        os.sleep(1000)
+        os.touch("src/foo.rs", {mtime = os.time()})
+        t:build()
+        assert(os.mtime(targetfile) > mtime, "target file should be rebuilt after rust dependency changes!")
     else
         return t:skip("wrong host platform")
     end
